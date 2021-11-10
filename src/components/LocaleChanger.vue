f1eeec7d500a76833d2c14edabbe8e903d4aba15
Mohammad Emami <emamirazavi@gmail.com>
2021-11-10 20:00:12 +0330

    modifying language icon interface

--- src/components/LocaleChanger.vue
+++ src/components/LocaleChanger.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="locale-changer" style="float:right">
-    
- <b-dropdown size="lg"  variant="link" toggle-class="text-decoration-none" no-caret v-model="$i18n.locale" v-on:change="localeChanged()">
+ <b-dropdown size="sm" variant="link" toggle-class="text-decoration-none" no-caret v-model="$i18n.locale" v-on:change="localeChanged()">
     <template #button-content>
-      <font-awesome-icon icon="language" size="2x" viewBox="0 0 640 305" onclick="document.getElementById('shastepa').triggerEvent('click')"/>
+      <font-awesome-icon icon="language" size="2x" viewBox="150 100 350 350" onclick="document.getElementById('shastepa').triggerEvent('click')"/>
     </template>
     <b-dropdown-item href="#" :key="`locale-${locale}`" v-for="locale in $i18n.availableLocales" v-on:click="localeChanged(locale)">{{ locale }}</b-dropdown-item>
   </b-dropdown>
 
   </div>
 </template>
+
+<style>
+/* .dropdown-toggle { */
+.locale-changer button{
+  padding: 0 !important;
+}
+</style>
 
 <script>
 export default {
